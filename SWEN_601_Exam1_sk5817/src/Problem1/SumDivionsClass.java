7fdc1f262e7fd98febca03be8ca4d0461b741cc7
Sagar Kate <sk5817@rit.edu>
2023-02-28 21:59:59 -0500

    Problem 1

---
+++ SWEN_601_Exam1_sk5817/src/Problem1/SumDivionsClass.java
@@ -0,0 +1,42 @@
+package Problem1;
+
+import java.util.Scanner;
+
+public class SumDivionsClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+     //   int num=10;
+        String choice ;
+
+         do{
+             System.out.println("Enter Number greater than 0 (positive) : ");
+             int num = sc.nextInt();
+            if(num>0)
+            {
+                System.out.println("The Sum is : "+ SumDivisions(num));
+            }
+            else{
+                System.out.println("Invalid Input");
+            }
+            System.out.println(" Do you want to continue  (y/Y) :  ");
+            choice = sc.next();
+        }while("Y".equalsIgnoreCase(choice));
+    }
+    /*
+    function name : SumDivisions
+    arg : int
+    return :  float
+    desc :  performs the summation as mentioned in the problem and returns the sum
+     */
+    private static float SumDivisions(int num) {
+        float sum =0;
+        for(float i=1;i<=num;i++)
+        {
+            sum+=1/i;
+            //System.out.println(i + "  "+sum );
+        }
+        return sum;
+    }
+}
